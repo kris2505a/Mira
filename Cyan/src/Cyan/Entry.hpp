8ec4724a3daa655e095f93d6571e0b5163b3cabd
Krishna <krishfps9222@gmail.com>
2025-02-13 00:39:48 +0530

    adding premake failed. So switched back to visual studio

--- Cyan/src/Cyan/Entry.hpp
+++ Cyan/src/Cyan/Entry.hpp
@@ -8,8 +8,8 @@ extern Cyan::Game* Cyan::CreateGame();
 int main(int argc, char** argv) {
 
 	Cyan::Log::init();
-	CYAN_LOG(CY_ERROR, "YAY");
-	LOG(CY_FATAL, "Game YAY");
+	CYAN_LOG(CY_INFO, "YAY");
+	LOG(CY_INFO, "Game YAY");
 	auto game = Cyan::CreateGame();
 	game->run();
 	delete game;
