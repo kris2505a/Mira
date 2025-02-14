4557d4708a385141ba8c1c96a9ff3da7bc6d0f5f
Krishna <krishfps9222@gmail.com>
2025-02-14 23:31:38 +0530

    avalothan

--- Cyan/src/Cyan/Game.cpp
+++ Cyan/src/Cyan/Game.cpp
@@ -17,12 +17,9 @@ namespace Cyan {
 	void Game::run() {
 
 		while (m_isRunning) {
-<<<<<<< HEAD
-=======
 
 			glClearColor(0.f, 1.f, 1.f, 1.f);
 			glClear(GL_COLOR_BUFFER_BIT);
->>>>>>> e40bbf89e139c116bc56c7f3a6e031dd760d9cf0
 			m_window->onUpdate();
 		}
 	}
