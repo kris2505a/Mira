e08a594313c906959be3895889e123e1a070ab2c
Krishna <krishfps9222@gmail.com>
2025-08-02 22:14:38 +0530

    completed camera component

--- MiraEngine/src/MiraEngine/Entity/Components.h
+++ MiraEngine/src/MiraEngine/Entity/Components.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <MiraEngine/Core/MiraCore.h>
 #include <MiraEngine/Math/Vector.h>
+#include <MiraEngine/Core/GameApp.h>
 
 namespace Mira {
 
@@ -16,8 +17,13 @@ struct MIRA_API CameraComponent {
 	Vec2Df position;
 	Vec2Df size;
 	float zoom;
+	sf::View camera;
 	CameraComponent(const Vec2Df& _position = {0.0f, 0.0f}, const Vec2Df& _size = {100.f, 100.f}, float _zoom = 1.0f) 
-		: position(_position), size(_size), zoom(_zoom) {}
+		: position(_position), size(_size), zoom(_zoom) {
+		camera.setSize(size);
+		camera.setCenter(position);
+		camera.zoom(zoom);
+	}
 };
 
 } //namespace ends
