ef7a5f78fba9f8eb9bbaaaacf8e9d2e4f15ed29b
Krishna <krishfps9222@gmail.com>
2025-08-14 15:39:17 +0530

    made engine be reused

--- premake5.lua
+++ premake5.lua
@@ -83,67 +83,67 @@ project "MiraEngine"
         runtime "Release"
         optimize "On"
 
-project "Sandbox"
-    location "Sandbox"
-    kind "consoleApp"
-    language "C++"
-    staticruntime "Off"
+-- project "Sandbox"
+--     location "Sandbox"
+--     kind "consoleApp"
+--     language "C++"
+--     staticruntime "Off"
 
 
-    targetdir("bin/" .. outputDir .. "/%{prj.name}")
-    objdir("obj/" .. outputDir .. "/%{prj.name}")
+--     targetdir("bin/" .. outputDir .. "/%{prj.name}")
+--     objdir("obj/" .. outputDir .. "/%{prj.name}")
 
-    files {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
+--     files {
+--         "%{prj.name}/src/**.h",
+--         "%{prj.name}/src/**.cpp"
+--     }
 
-    includedirs {
-        "%{IncludeDirectories.SFML}/include",
-        "%{prj.name}/src",
-		"MiraEngine/src/"
-    }
+--     includedirs {
+--         "%{IncludeDirectories.SFML}/include",
+--         "%{prj.name}/src",
+-- 		"MiraEngine/src/"
+--     }
 
-    libdirs {
-		"%{IncludeDirectories.SFML}/lib"
-    }
+--     libdirs {
+-- 		"%{IncludeDirectories.SFML}/lib"
+--     }
     
-    links {
-        "opengl32.lib",
-		"winmm.lib",
-		"gdi32.lib",
-		"MiraEngine",
-    }
+--     links {
+--         "opengl32.lib",
+-- 		"winmm.lib",
+-- 		"gdi32.lib",
+-- 		"MiraEngine",
+--     }
 
-    filter "system:windows"
-        cppdialect "C++20"
-        staticruntime "Off"
-        systemversion "latest"
+--     filter "system:windows"
+--         cppdialect "C++20"
+--         staticruntime "Off"
+--         systemversion "latest"
 
-    defines {
+--     defines {
 	
-    }
+--     }
 
-    filter "configurations:Debug" 
-        links {
-            "sfml-audio-d.lib",
-            "sfml-graphics-d.lib",
-            "sfml-window-d.lib",
-            "sfml-main-d.lib",
-            "sfml-system-d.lib"
-        }
-        runtime "Debug"
-        symbols "On"
+--     filter "configurations:Debug" 
+--         links {
+--             "sfml-audio-d.lib",
+--             "sfml-graphics-d.lib",
+--             "sfml-window-d.lib",
+--             "sfml-main-d.lib",
+--             "sfml-system-d.lib"
+--         }
+--         runtime "Debug"
+--         symbols "On"
 
-    filter "configurations:Release"
-        links {
-            "sfml-audio.lib",
-            "sfml-graphics.lib",
-            "sfml-window.lib",
-            "sfml-main.lib",
-            "sfml-system.lib"
-        }
-        runtime "Release"
-        optimize "On"
+--     filter "configurations:Release"
+--         links {
+--             "sfml-audio.lib",
+--             "sfml-graphics.lib",
+--             "sfml-window.lib",
+--             "sfml-main.lib",
+--             "sfml-system.lib"
+--         }
+--         runtime "Release"
+--         optimize "On"
 
 	
